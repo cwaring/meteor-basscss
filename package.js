3b5062df1cadf712fd9d66860aa457f691c8444f
Chris Waring <chris@wwaves.co>
2015-04-07 22:08:16 -0700

    update to basscss 5.2.0 & cssnext compiler

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kit:basscss',
-  version: '5.0.1',
+  version: '5.2.1',
   summary: 'Basscss packaged for Meteor',
   git: 'https://github.com/cwaring/meteor-basscss.git',
   documentation: 'README.md'
