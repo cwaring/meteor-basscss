58dab057dd8b00c8c0eeb3142e2794c85f756ddc
Chris Waring <chris@wwaves.co>
2015-04-30 15:54:26 -0700

    6.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kit:basscss',
-  version: '5.2.1',
+  version: '6.0.2',
   summary: 'Basscss packaged for Meteor',
   git: 'https://github.com/cwaring/meteor-basscss.git',
   documentation: 'README.md'
